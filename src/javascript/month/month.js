c19e737df521635ca83e8a6f53eb5b2f6933b412
lirongkan <33339324+lirongkan@users.noreply.github.com>
2019-04-09 23:43:44 +0800

    month exercise

--- src/javascript/month/month.js
+++ src/javascript/month/month.js
@@ -1,30 +1,14 @@
-function showDay(){
-    var inputObj =document.getElementById("month");
-    var month = inputObj.value;
+function getMonth() {
 
-    month =parseInt(month);
-    switch(month){
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            alert("本月有31天");
-            break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            alert("本月有30天");
-            break;
-        case 2:
-            alert("闰年29天，非闰年28天");
-            break;
-        default:
-            alert("没有该月份！");
-            break;
-
+    var char = document.getElementById("month");
+        var str =char.value;
+    if(str==1||str==3||str==5||str==7||str==8||str==10||str==12){
+        alert(31);
+    }else if(str==4||str==6||str==9||str==11){
+        alert(30);
+    }else if(str == 2){
+        alert(28);
+    }else{
+        alert("没有该月份！");
     }
 }
